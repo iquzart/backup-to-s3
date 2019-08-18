169fe54bd3d546cd03b63e14eb05fc7795011547
Muhammed Iqbal <iquzart@hotmail.com>
2019-08-18 16:09:11 +0400

    enabled logging

--- archive.py
+++ archive.py
@@ -1,35 +1,38 @@
 import os
 import time
 import tarfile
-from hurry.filesize import size
+import logging
 from S3_upload import upload
 
 
-
 def archive():
-    archive_location = '/mnt/'
+    archive_location = '/tmp/'
     file_name = 'backup-'+time.strftime("%Y%m%d-%H%M%S")+'.tar.gz'
-    #archive_source = os.environ["ARCHIVE_SOURCE"]
-    archive_source = '/home/iqbal/Downloads'
+    archive_source = os.environ["ARCHIVE_SOURCE"]
+    bucket_name = os.environ["BUCKET_NAME"]
 
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.INFO)
 
-    try: 
+    try:
+        logging.info('archive process started - sit tight!')
         with tarfile.open(archive_location+file_name, mode='w:gz') as archive:
              archive.add(archive_source, recursive=True)
+
     except OSError as error:
-        print ("Archive Failed!")
-        print (error)
-    else: 
-         
-         print ("Backup file created:",archive_location+file_name)
-         print ("Archive file size: "'{:,.0f}'.format(os.path.getsize(archive_location+file_name)/float(1<<20))+" MB")
-#         print (os.path.getsize(archive_location+file_name)/ (1024*1024.0))
-
-         print ("Uploading backup to s3 bucket")
-         upload(archive_location, file_name)
+        logging.error('archive failed')
+        logging.error('%s',error)
+    else:
+         size = round(os.path.getsize(archive_location+file_name)/(1024*1024.0))
+         logging.info('archive has been completed - %s%s - size: %sM',archive_location,file_name,size)
 
 
-         print ("Clearing the local backup file:",archive_location+file_name) 
-         os.remove(archive_location+file_name)
-    
+         logging.info('uploading backup to s3 bucket - %s',bucket_name)
+#         upload(archive_location, file_name)
+
+
+         logging.info('clearing the local backup file - %s%s',archive_location,file_name)
+#         os.remove(archive_location+file_name)
+
 archive ()
